8c6ef0e06bf4dcd9ad50b4c9fed8949c1717c0bf
Tareef Iman Azizi <tareef.iman.azizi@gmail.com>
2021-09-09 09:21:48 +0800

    updated
    division still not working

--- calculator.js
+++ calculator.js
@@ -1,33 +1,40 @@
-const operations = {
+var readlineSync = require('readline-sync');
+
+/* these are the operators*/
+const values = { 
   '+': function(a, b) { return a + b },
   '-': function(a, b) { return a - b },
   '*': function(a, b) { return a * b },
   '/': function(a, b) { return a / b }
 }
 
-function operate() {
-  let done = false;
-  operations.prompt (`Enter a problem to solve. You may use + - * /. Make sure there's a space between each character. 
-  \nPress enter when ready.`);
-  while (!done) {
-    const add = prompt('\nsolve a problem: ');
-    let answer = add.split(' ');
+function run(){
+  let question1 = "";
+   let operation = false;
 
-    if (answer.length === 3) {
-      let a = parseFloat(answer[0]);
-      let b = parseFloat(answer[2]);
-      if (answer[1] === '/') {
-        console.log(operations[answer[1]](a, b).toFixed(2));
-        done = true;
-      } else {
-        console.log(operations[answer[1]](a, b));
-        done = true;        
-      }
-    } else {
-      console.log(`Put a space between the numbers and operation and try again. `);
-    }
+while(!operation) {
+  question1 = readlineSync.question('What operation would you like to perform? (+, -, *, /) ');
+  if (question1 === '+'
+      || question1 === '-'
+      || question1 === '/'
+      || question1 === '*') {
+    operation = true;
+    break;
+  } else {
+    console.log('That is not a valid operation!');
   }
 }
+  var question2= readlineSync.questionInt('Please enter the first number')
+  var question3= readlineSync.questionInt('Please enter the second number');
+  var total = question2 + question1 + question3;
+  let answer = total.split('')
 
-operate();
-
+  if( answer.length === 3 ){
+      let a = parseFloat(answer[0]);
+      let b = parseFloat(answer[2]);
+      var end = "The result is ";
+        console.log( end + values[answer[1]](a, b));
+    } 
+  
+  }
+run()
